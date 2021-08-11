7c3e5db8fe1b1ef24cf40ba49d7052fb900a681f
Madhuri <harshupatil0301@gmail>
2021-08-11 06:17:47 +0530

    [Madhuri]Add : Creating A Company Page

--- js/footer.js
+++ js/footer.js
@@ -1,37 +1,38 @@
 console.log("hellofooter.js");
 document.getElementById('footer').innerHTML = `
-<div class="footer" >
-<div class="footer-content">
-        <div class="logo2">
-            <img src="../assets/footerlogo.png" alt="logo-image">
-        </div>
-        <div class="content1">
-            <ul>
-                <li> GET IN TOUCH </li>
-                <li>(480)-624-5200</li>
-                <li><a class="mail" href="">mail@discoverylandco.com"</a></li>
-
-            </ul>
-        </div>
-        <div class="content2">
-            <ul>
-                <li>About DLC </li>
-                <li>Our Worlds </li>
-                <li> Experiences</li>
-                <li> Gallery </li>
-                <li>Press </li>
-
-            </ul>
-        </div>
-        <div class="content3">
-            <ul>
-                <li>Magazine</li>
-                <li>Carrers</li>
-                <li>Foundation</li>
-                <li>Company</li>
-                <li>Contact</li>
-                <li>Privacy</li>
-            </ul>
-        </div>
-        </div>
-    </div>`
+<footer class="footer">
+        <!-- logo -->
+       <div class="wrap">
+           <a href="#!"><img class="logo" src="../assets/footerlogo.png" /></a>     
+       </div> 
+       <div class="container">     
+           <div class="row">
+               <div class="footer-col">
+    
+                   <h4>Get In Touch</h4>
+                   <ul class="list-container">
+                       <li><a href="#">(480) 6245220</a></li>
+                       <li><a href="#">mail@Discoverylandco.com</a></li>
+                   </ul>
+               </div>
+           <div class="footer-col">
+               <ul class="list-container">
+                   <li><a href="#">About DLC</a></li>
+                   <li><a href="#">Our World</a></li>
+                   <li><a href="#">Experiences</a></li>
+                   <li><a href="#">Gallery</a></li>
+                   <li><a href="#">Press</a></li>
+               </ul>
+           </div>
+           <div class="footer-col">
+               <ul class="list-container">
+                   <li><a href="#">Magazine</a></li>
+                   <li><a href="#">Careers</a></li>
+                   <li><a href="#">Foundation</a></li>
+                   <li><a href="#">Company</a></li>
+                   <li><a href="#">Contact</a></li>
+                   <li><a href="#">Privacy</a></li>
+               </ul>
+           </div>
+       </div>         
+   </footer>`
