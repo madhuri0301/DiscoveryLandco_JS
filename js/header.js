4bc30ef3a0a50d7cb378815dff98746a6715a4c5
Madhuri <harshupatil0301@gmail>
2021-08-13 11:08:04 +0530

    [Madhuri]Refactor : Creating a Home Page

--- js/header.js
+++ js/header.js
@@ -41,19 +41,19 @@ document.getElementById("header").innerHTML = `
          </div>
          </div>
                     <div class="a-div">
-                        <a class="drpdwn-a" href="../pages/magazine.html">Magazine</a>
+                        <a class="drpdwn-a" href="/pages/magzine.html">Magazine</a>
                     </div>
                     <div class="a-div">
-                        <a class="drpdwn-a" href="../pages/company.html">Company</a>
+                        <a class="drpdwn-a" href="/pages/company.html">Company</a>
                     </div>
                     <div class="a-div">
-                        <a class="drpdwn-a" href="../pages/foundation.html">Foundation</a>
+                        <a class="drpdwn-a" href="/pages/foundation.html">Foundation</a>
                     </div>
                     <div class="a-div">
-                        <a class="drpdwn-a" href="../pages/careers.html">Careers</a>
+                        <a class="drpdwn-a" href="/pages/careers.html">Careers</a>
                     </div>
                     <div class="a-div">
-                        <a class="drpdwn-a" href="../pages/contact.html">Contact</a>
+                        <a class="drpdwn-a" href="/pages/contact.html">Contact</a>
                     </div>
                 </div>`;
 
